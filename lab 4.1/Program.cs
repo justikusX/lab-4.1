af52cd8de06908fb67cd60ba47e3c048e42d77b9
JustikusX <JustikusX@209-14>
2024-11-08 18:01:09 +0200

    Добавьте файлы проекта.

---
+++ lab 4.1/Program.cs
@@ -0,0 +1,19 @@
+﻿double [] array = { 1.5, 2.5, 3.4, 5.3, 7.2 };
+Console.WriteLine("введите число R");
+double R = int.Parse(Console.ReadLine());
+double C_V = array[0];
+int C_I = 0;
+double min_Diff = Math.Abs(array[0] - R);
+for (int i = 1; i < array.Length; i++)
+{
+    double C_D =Math.Abs(array[i]-R);
+    if (C_D < min_Diff)
+    {
+        min_Diff = C_D;
+        C_V = array[i];
+        C_I = i;
+    }
+    Console.WriteLine();
+
+}
+Console.WriteLine($"Ближайшее число:{C_V} \n Индекс: {C_I}");
